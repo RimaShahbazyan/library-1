d14667d93dea69ab030d1c570412e36f6754dd59
rima_shahbazyan <s.rima2000rs@gmail.com>
2019-08-14 12:48:13 +0400

    Committing Issue Page Half-done

--- Library/pages/IssueAbook/issue.controller.js
+++ Library/pages/IssueAbook/issue.controller.js
@@ -1,3 +1,8 @@
 function reserveBook (  ) {
-    fromDate = window.document.p
+    let fromDate = window.document.getElementById("fromDate").value;
+    let toDate = window.document.getElementById("toDate").value;
+    let bookId = localStorage.getItem("bookId");
+    let book = lms.bookManagementService.getBookById(bookId);
+    let user =  sessionStorage.getItem("authInfo")["user"];
+    window.lms.issueService.addNewIssue(book, )
 }
